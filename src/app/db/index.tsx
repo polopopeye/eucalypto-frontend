817e8149ce206ffdb99e4780316accaba80502a3
kenneth suarez <hardmonkeypc@gmail.com>
2022-04-07 02:35:19 +0200

    update

--- src/app/db/index.tsx
+++ src/app/db/index.tsx
@@ -1,8 +1,15 @@
-import credential from "./firebase.json";
-
 import firebase from "firebase/app";
+
 export const iniDB = () => {
-  firebase.initializeApp(credential);
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  });
 };
 export default iniDB;
 
