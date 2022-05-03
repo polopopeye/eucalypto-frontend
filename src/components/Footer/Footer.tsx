ea90c6988e4c201876fe80c0b6b907c7e0ee0466
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-03 07:27:05 +0200

    build error solve

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <div>
       <div className="flex  mx-auto w-full place-content-center mt-16 pb-16">
-        <div className="text-footerBrand  px-1 mx-1 text-xs">Made With</div>
-        TEEEEEST
+        <div className="text-footerBrand  px-1 mx-1 text-xs">
+          Hi Im a footer
+        </div>{" "}
+        yes
       </div>
     </div>
   );
