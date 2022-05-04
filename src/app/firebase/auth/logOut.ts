5b82c31c3fd4450c9365bec5781d43d4c3e3cb10
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-04 19:30:13 +0200

    test

--- src/app/firebase/auth/logOut.ts
+++ src/app/firebase/auth/logOut.ts
@@ -1,20 +1,22 @@
-import { getAuth, signOut } from "firebase/auth";
-import userSlice from "../../slices/user/userSlice";
-import { store } from "../../store";
-import { toast } from "react-toastify";
-import { Router, useRouter } from "next/router";
+import { getAuth, signOut } from 'firebase/auth';
+import userSlice from '../../slices/user/userSlice';
+import { store } from '../../store';
+import { toast } from 'react-toastify';
 
-const logOut = () => {
+const logOut = (next?: Function) => {
   const auth = getAuth();
 
   signOut(auth)
     .then((response) => {
       store.dispatch(userSlice.actions.emptyData());
-      toast.success("Bye, see you soon!");
+      toast.success('Bye, see you soon!');
+      if (next) {
+        next();
+      }
     })
     .catch((error) => {
-      console.log("🚀 ~ file: logOut.ts ~ line 12 ~ error", error);
-      toast.error("Error logging out");
+      console.log('🚀 ~ file: logOut.ts ~ line 12 ~ error', error);
+      toast.error('Error logging out');
     });
 };
 
