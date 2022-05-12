a3d8733ed9c7430bdd60ed963d13b7e36b7aac4e
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-12 22:12:34 +0200

    Update Hero.tsx

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -11,68 +11,63 @@ export default function Hero(props: HeroProps) {
   const { title, titleFeatured, description, href, btnText, src } = props;
 
   return (
-    <div className="relative bg-white overflow-hidden mb-2">
-      <div className=" mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
+    <div className="relative bg-white overflow-hidden mb-4">
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        className=" absolute -mt-12 left-0 hidden lg:block"
+        style={{
+          width: '100% !important',
+          height: 'auto',
+        }}
+      >
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/eucalypto-group.appspot.com/o/production%20ID_4478322.mp4?alt=media&token=b56364a6-c707-4e2c-b5dc-ddee6f45eac5"
+          type="video/mp4"
+        />
+        Tu navegador no soporta el formato de video
+      </video>
 
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl  pt-16 pb-16">
-                <span className="block xl:inline">{title}</span>
-                <span className="block text-primary xl:inline">
-                  {titleFeatured}
-                </span>
-              </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                {description}
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  {btnText && (
-                    <a
-                      href={href}
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-secondary md:py-4 md:text-lg md:px-10"
-                    >
-                      {btnText}
-                    </a>
-                  )}
-                </div>
+      <div
+        style={{ background: '#ffffff96' }}
+        className="ml-4 mb-4 relative z-10 pb-8   sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+      >
+        <svg
+          className="hidden lg:block text-primary absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+          fill="currentColor"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          aria-hidden="true"
+        >
+          <polygon points="50,0 100,0 50,100 0,100" />
+        </svg>
+
+        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <div className="sm:text-center lg:text-left">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl  pt-16 pb-16">
+              <span className="block xl:inline">{title}</span>
+              <span className="block text-primary xl:inline">
+                {titleFeatured}
+              </span>
+            </h1>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              {description}
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                {btnText && (
+                  <a
+                    href={href}
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-secondary md:py-4 md:text-lg md:px-10"
+                  >
+                    {btnText}
+                  </a>
+                )}
               </div>
             </div>
-          </main>
-        </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <video
-          autoPlay={true}
-          muted={true}
-          loop={true}
-          className=" fixed -mt-12 left-0"
-          style={{
-            width: '100% !important',
-            height: 'auto',
-          }}
-        >
-          <source
-            src="https://firebasestorage.googleapis.com/v0/b/eucalypto-group.appspot.com/o/production%20ID_4478322.mp4?alt=media&token=b56364a6-c707-4e2c-b5dc-ddee6f45eac5"
-            type="video/mp4"
-          />
-          Tu navegador no soporta el formato de video
-        </video>
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={src}
-          alt={title}
-        />
+          </div>
+        </main>
       </div>
     </div>
   );
