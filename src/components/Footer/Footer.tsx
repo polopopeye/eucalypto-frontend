1582ba204a96a018ef857e4330828336148ab2a0
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-13 08:22:14 +0200

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <div>
       <div className="flex  mx-auto w-full place-content-center mt-16 pb-16">
         <div className="text-footerBrand  px-1 mx-1 text-xs">
-          Hi Im a footer
-        </div>{' '}
-        yes
+          Eucalypto Group Innovation S.l. © 2022, All rights reserved -{' '}
+          <a href="https://www.eucalyptogroup.com/contact">Contact</a>
+        </div>
       </div>
     </div>
   );
