b828ca658924c498dfbbb30bb3668d477fc0e1a7
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-25 11:28:37 +0200

    login with github

---
+++ src/app/firebase/auth/registerGithub.ts
@@ -0,0 +1,53 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from 'firebase/auth';
+import registerUserInBackend from '../../backend/users/registerUserInBackend';
+import { toast } from 'react-toastify';
+import { errors } from '../constants';
+import retrieveUserInfo from 'src/app/backend/users/retrieveUserInfo';
+import newUpload from '../storage/newUpload';
+
+const registerGithub = async () => {
+  const auth = getAuth();
+  const provider = new GithubAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(
+        '🚀 ~ file: registerGithub.ts ~ line 20 ~ .then ~ user',
+        user
+      );
+      const { email, displayName } = user;
+      const nameId = (displayName as string) || (email as string);
+
+      registerUserInBackend(
+        {
+          completeName: nameId,
+          displayName: nameId,
+          languages: ['english'],
+          role: 'talent',
+          coverImg: '/file/img/Icono_Negativo.png',
+          email: email as string,
+          published: true,
+        },
+        (user: any) => {
+          retrieveUserInfo({
+            prop: 'email',
+            value: user.email as string,
+          });
+        }
+      );
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = errors[errorCode];
+      toast.error(errorMessage);
+      return errorMessage;
+    });
+};
+
+export default registerGithub;
