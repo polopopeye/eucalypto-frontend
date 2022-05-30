a7e49dd964e310e807d21bb9c8c84e1943cae988
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-30 02:57:21 +0200

    update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,28 @@
 // fontFamily: {
 //   sans: ['Inter var', ...defaultTheme.fontFamily.sans],
 // },
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        tertiary: "var(--color-tertiary)",
-        quaternary: "var(--color-quaternary)",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        quaternary: 'var(--color-quaternary)',
         rose: colors.rose,
+      },
+      fontFamily: {
+        sans: ['Poppins'],
+        poppins: ['Poppins'],
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
